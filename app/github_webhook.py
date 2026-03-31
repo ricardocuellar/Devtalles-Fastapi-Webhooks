475fae0db595c610612f8efd07c15eba1dcd8f92
Ricardo Cuéllar <rcuellarsn@gmail.com>
2026-03-30 19:45:35 -0600

    Add github route

--- app/github_webhook.py
+++ app/github_webhook.py
@@ -1,8 +1,15 @@
 """Importar librerias"""
+from datetime import datetime
 import hmac
 import hashlib
 import os
 
+from fastapi import APIRouter, Header, HTTPException, Request, Depends
+from sqlmodel import Session, select
+from app.db import get_session
+from .models import Issue
+
+router = APIRouter()
 
 GITHUB_SECRET = os.getenv("GITHUB_SECRET", "secretosuper123")
 
@@ -30,3 +37,42 @@ def verify_signature(body: bytes, signature_header: str | None) -> bool:
     expected = mac.hexdigest()
 
     return hmac.compare_digest(expected, signature)
+
+
+@router.post("/webhooks/github")
+async def github_webhook(
+    request: Request,
+    x_hub_signature_256: str | None = Header(default=None),
+    x_github_event: str | None = Header(default=None),
+    session: Session = Depends(get_session)
+):
+    """Endpoint que recibe los webhooks de Github"""
+    body = await request.body()
+
+    if not verify_signature(body, x_hub_signature_256):
+        raise HTTPException(status_code=401, detail="Firma inválida")
+
+    payload = await request.json()
+
+    if x_github_event == "issues" and payload.get("action") == "opened":
+        issue_data = payload["issue"]
+        created_at_str = issue_data["created_at"]  # "2026-01-20T10:00:00Z"
+        created_at = datetime.fromisoformat(
+            created_at_str.replace("Z", "+00:00")
+        )
+
+        issue = Issue(
+            github_issue_id=issue_data["id"],
+            title=issue_data["title"],
+            url=issue_data["html_url"],
+            state=issue_data["state"],
+            created_at=created_at
+        )
+
+        session.add(issue)
+        session.commit()
+        session.refresh(issue)
+
+        print(f"NUEVO issue guardado: {issue.title} - {issue.url}")
+
+    return {"status": "ok"}
